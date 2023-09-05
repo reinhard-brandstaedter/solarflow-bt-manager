29c4a9ddc892dbbedd53f8a2176242fdd8c8e87b
Reinhard Weber <r.brandstaedter@gmail.com>
2023-09-05 13:07:41 +0200

    getinfo every minute not hour

--- src/test-bt.py
+++ src/test-bt.py
@@ -142,7 +142,7 @@ async def run(broker=None, port=None, info_only: bool = False, connect: bool = F
         else:
             while True:
                 # fetch global info every minute
-                if int(time.strftime("%M")) == 0:
+                if int(time.strftime("%S")) == 0:
                     await getInfo(bt_client)
                 await bt_client.start_notify(SF_NOTIFY_CHAR,handle_rx)
 

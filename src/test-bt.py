dbd7eb7c2df2e97d0c4c2b1135b17eff5703c1dc
Reinhard Weber <r.brandstaedter@gmail.com>
2023-09-05 11:06:28 +0200

    refactoring

--- src/test-bt.py
+++ src/test-bt.py
@@ -4,8 +4,9 @@ from paho.mqtt import client as mqtt_client
 import json
 import logging
 import sys
+import getopt
 import os
-from time import time 
+import time 
 
 FORMAT = '%(asctime)s:%(levelname)s: %(message)s'
 logging.basicConfig(stream=sys.stdout, level="INFO", format=FORMAT)
@@ -15,15 +16,12 @@ log = logging.getLogger("")
 Very basic attempt to just report Solarflow Hub's stats to mqtt for local long-term tests
 '''
 
+SF_GATT_CHAR = "0000c304-0000-1000-8000-00805f9b34fb"
+
 address = os.environ.get('SF_BT_ADDR',"94:C9:60:3E:C8:E7")
-MQTT_HOST = os.environ.get('MQTT_HOST',"192.168.1.245")
-MQTT_PORT = os.environ.get('MQTT_PORT',1883)
 WIFI_PWD = os.environ.get('WIFI_PWD',None)
 WIFI_SSID = os.environ.get('WIFI_SSID',None)
-SF_DEVICE_ID = os.environ.get('SF_DEVICE_ID',None)
-local_broker = MQTT_HOST
-local_port = MQTT_PORT
-local_client: mqtt_client
+mq_client: mqtt_client
 
 
 def on_connect(client, userdata, flags, rc):
@@ -32,60 +30,53 @@ def on_connect(client, userdata, flags, rc):
     else:
         log.error("Failed to connect, return code %d\n", rc)
 
-async def local_mqtt_connect():
-    global local_client
-    global local_port
-    local_client = mqtt_client.Client(client_id="solarflow-bt")
-    local_client.connect(local_broker,local_port)
-    local_client.on_connect = on_connect
+def local_mqtt_connect(broker, port):
+    client = mqtt_client.Client(client_id="solarflow-bt")
+    client.connect(broker,port)
+    client.on_connect = on_connect
+    return client
 
 async def getInfo(client):
-    global local_client
-    char = "0000c304-0000-1000-8000-00805f9b34fb"
-    info_cmd = {"deviceId":"5ak8yGU7","messageId":"none","method":"getInfo","timestamp": str(int(time())) }
-    properties_cmd = {"method":"read", "timestamp": str(int(time())), "messageId": "none","deviceId": "5ak8yGU7","properties":["getAll"]}
+    info_cmd = {"deviceId":"5ak8yGU7","messageId":"none","method":"getInfo","timestamp": str(int(time.time())) }
+    properties_cmd = {"method":"read", "timestamp": str(int(time.time())), "messageId": "none","deviceId": "5ak8yGU7","properties":["getAll"]}
 
     try:
         b = bytearray()
         b.extend(map(ord, json.dumps(info_cmd)))
-        await client.write_gatt_char(char,b,response=False)
+        await client.write_gatt_char(SF_GATT_CHAR,b,response=False)
     except Exception:
         log.exception("Getting device Info failed")
     
     try:
         b = bytearray()
         b.extend(map(ord, json.dumps(properties_cmd)))
-        await client.write_gatt_char(char,b,response=False)
+        await client.write_gatt_char(SF_GATT_CHAR,b,response=False)
     except Exception:
         log.exception("Getting device Info failed")
 
 def set_IoT_Url(client):
-    global local_client
-    char = "0000c304-0000-1000-8000-00805f9b34fb"
+    global mq_client
     cmd1 = '{"iotUrl":"{}","messageId":"1002","method":"token","password":"{}","ssid":"{}","timeZone":"GMT+08:00","token":"abcdefgh"}'.format(MQTT_HOST,MQTT_PORT,WIFI_PWD,WIFI_SSID)
-
     cmd2 = '{"messageId":"1003","method":"station"}'
 
-    reply = '{"messageId":123,"timestamp":'+str(int(time()))+',"params":{"token":"abcdefgh","result":0}}'
+    reply = '{"messageId":123,"timestamp":'+str(int(time.time()))+',"params":{"token":"abcdefgh","result":0}}'
     
     try:
         b = bytearray()
         b.extend(map(ord, cmd1))
-        client.write_gatt_char(char,b,response=False)
+        client.write_gatt_char(SF_GATT_CHAR,b,response=False)
     except Exception:
         log.exception("Setting reporting URL failed")
 
-    
     try:
         b = bytearray()
         b.extend(map(ord, cmd2))
-        client.write_gatt_char(char,b,response=False)
+        client.write_gatt_char(SF_GATT_CHAR,b,response=False)
     except Exception:
         log.exception("Setting WiFi Mode failed")
 
 
-    local_client.publish("iot/73bkTV/5ak8yGU7/register/replay",reply)
-    
+    mq_client.publish("iot/73bkTV/5ak8yGU7/register/replay",reply)
 
 
 def handle_rx(BleakGATTCharacteristic, data: bytearray):
@@ -99,10 +90,10 @@ def handle_rx(BleakGATTCharacteristic, data: bytearray):
         props = payload["properties"]
 
         for prop, val in props.items():
-            local_client.publish(f'solarflow-hub/telemetry/{prop}',val)
+            mq_client.publish(f'solarflow-hub/telemetry/{prop}',val)
 
         # also report whole state to mqtt (nothing coming from cloud now :-)
-        local_client.publish("SKC4SpSn/5ak8yGU7/state",json.dumps(payload["properties"]))
+        mq_client.publish("SKC4SpSn/5ak8yGU7/state",json.dumps(payload["properties"]))
     
     if "packData" in payload:
         packdata = payload["packData"]
@@ -110,11 +101,11 @@ def handle_rx(BleakGATTCharacteristic, data: bytearray):
             for pack in packdata:
                 sn = pack.pop('sn')
                 for prop, val in pack.items():
-                    local_client.publish(f'solarflow-hub/telemetry/batteries/{sn}/{prop}',val)
+                    mq_client.publish(f'solarflow-hub/telemetry/batteries/{sn}/{prop}',val)
 
 
-async def main():
-
+async def run(broker=None, port=None, info_only: bool = False, connect: bool = False, disconnect: bool = False):
+    global mq_client
     device = await BleakScanner.find_device_by_filter(
                 lambda d, ad: d.name and d.name.lower().startswith("zen")
             )
@@ -127,15 +118,68 @@ async def main():
         for service in svcs:
             log.info(service)
 
-        await local_mqtt_connect()
+        if broker and port:
+            mq_client = local_mqtt_connect(broker,port)
 
-        #await set_IoT_Url(client)
-
-        await getInfo(client)
+        if disconnect and broker and port:
+            await set_IoT_Url(client,broker,port)
 
         while True:
-            char = "0000c305-0000-1000-8000-00805f9b34fb"
-            await client.start_notify(char,handle_rx)
+            await getInfo(client)
+            await client.start_notify(SF_GATT_CHAR,handle_rx)
+            time.sleep(5)
+            if info_only:
+                break
 
+def main(argv):
+    global mqtt_user, mqtt_pwd
 
-asyncio.run(main())
+    ssid = None
+    mqtt_broker = None
+    opts, args = getopt.getopt(argv,"hidb:u:p:w:c")
+    for opt, arg in opts:
+        if opt == '-h':
+            print('solarflow-bt-manager.py [ -i | -d | -c ]')
+            print(' -i\tprint some information about the hub and exit')
+            print(' -d\tdisconnect the hub from Zendure cloud')
+            print(' -b\thostname:port of local MQTT broker')
+            print(' -w\tWiFi SSID the hub should be connected to')
+            print(' -d\tconnect the hub to Zendure cloud')
+            sys.exit()
+        elif opt in ("-i", "--info"):
+            info_only = True
+            disconnect = connect = False
+        elif opt in ("-d", "--disconnect"):
+            disconnect = True
+            connect = False
+        elif opt in ("-w", "--wifi"):
+            ssid = arg
+        elif opt in ("-b", "--mqtt_broker"):
+            parts = arg.split(':')
+            mqtt_broker = parts[0]
+            mqtt_port = parts[1] if len(parts) > 1 else 1883
+        elif opt in ("-u", "--mqtt_user"):
+            mqtt_user = arg
+        elif opt in ("-p", "--mqtt_pwd"):
+            mqtt_pwd = arg    
+        elif opt in ("-c", "--connect"):
+            connect = True
+            disconnect = False
+
+    if disconnect:
+        if ssid is None:
+            print("Disconnecting from Zendure cloud requires a WiFi SSID (-w)!")
+            sys.exit()
+        if mqtt_broker is None:
+            print("Disconnecting from Zendure cloud requires a local MQTT broker (-b)!")
+            sys.exit()
+        if WIFI_PWD is None:
+            print('Please provide password for WiFi SSID {ssid} via environment variable WIFI_PWD')
+    
+    if connect:
+        print("Connecting Solarflow Hub Back to Zendure Cloud")
+
+    asyncio.run(run(broker=mqtt_broker, port=mqtt_port, info_only=info_only, connect=connect, disconnect=disconnect))
+
+if __name__ == '__main__':
+    main(sys.argv[1:])

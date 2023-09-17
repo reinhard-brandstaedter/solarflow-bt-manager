89749f74536dd780d9b78bc9843cb9af3a598aaf
Reinhard Weber <r.brandstaedter@gmail.com>
2023-09-17 23:55:23 +0200

    logging

--- src/solarflow-bt-manager.py
+++ src/solarflow-bt-manager.py
@@ -142,11 +142,13 @@ async def run(broker=None, port=None, info_only: bool = False, connect: bool = F
 
         if disconnect and broker and port and ssid and SF_DEVICE_ID:
             await set_IoT_Url(bt_client,broker,port,ssid,SF_DEVICE_ID)
+            log.info("Setting IoTURL connection parameters - disconneect")
             await asyncio.sleep(30)
             return
         
         if connect and ssid:
             await set_IoT_Url(bt_client,"mq.zen-iot.com",1883,ssid,SF_DEVICE_ID)
+            log.info("Setting IoTURL connection parameters - connect")
             await asyncio.sleep(30)
             return
 
